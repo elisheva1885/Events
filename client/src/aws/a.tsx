37d6f8a0ae7c6b8a91e094a942e720d5caa24fcd
Ruth shtraicher <RutProgramming8978@gmail.com>
2025-11-23 09:48:36 +0200

    Removing unnecessary files

--- client/src/aws/a.tsx
+++
@@ -1,9 +0,0 @@
-import FileManager from "./FileManager";
-
-const files = [
-  { key: 'מערכות הפעלה קובץ נכון.pdf', name: 'תמונה 1', type: 'application/pdf' },
-];
-
-export default function A() {
-  return <FileManager files={files} />;
-}
