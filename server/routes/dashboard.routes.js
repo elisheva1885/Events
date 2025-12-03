9ffccee400103d8eba87e6822f6232568b798f0b
Miri Radunsky <miriradunsky@gmail.com>
2025-12-03 15:13:06 +0200

    admin architecture fix

--- server/routes/dashboard.routes.js
+++ server/routes/dashboard.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { authGuard } from '../middlewares/auth.middleware.js';
 import { DashboardController } from "../controllers/dashboard.controller.js";
+import { authGuard } from "../middlewares/auth.middleware.js";
 import { roleGuard } from "../middlewares/role.middleware.js";
+
 const router = Router();
 
-router.get("/summaryUser", authGuard,roleGuard(["user"]), DashboardController.getDashboardSummaryForUser);
-router.get("/summarySupplier", authGuard,roleGuard(["supplier"]), DashboardController.getDashboardSummaryForSupplier);
+router.use(authGuard);
+
+router.get("/summaryUser", roleGuard(["user"]), DashboardController.getDashboardSummaryForUser);
+
+router.get("/summarySupplier", roleGuard(["supplier"]), DashboardController.getDashboardSummaryForSupplier);
+router.get("/summaryAdmin", roleGuard(["admin"]), DashboardController.getDashboardSummaryForAdmin);
 
 export default router;
