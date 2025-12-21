93f5e1322abe9251d48f8dc1a938cb15c5ff981c
Ruth shtraicher <RutProgramming8978@gmail.com>
2025-12-21 03:12:59 +0200

    supplier register

--- server/controllers/supplier.controller.js
+++ server/controllers/supplier.controller.js
@@ -39,7 +39,7 @@ export const SupplierController = {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 1000 * 60 * 60 * 24 * 7,
     });
     return res.status(201).json({ success: true });
